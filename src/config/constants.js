2ca43ae2326437179e8eb84c0ac9e66c3fb40d76
Gabriela Rey <gabrp2908@gmail.com>
2025-11-10 18:23:11 -0400

    fix: default_scale and tile_size

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,8 +2,8 @@
 export const ASSET_PATH = "../assets/sprites/";
 export const GRAVITY = 0.3;
 export const TICKS_PER_FRAME = 8;
-export const DEFAULT_SCALE = 2;
-export const TILE_SIZE = 32;
+export const DEFAULT_SCALE = 1;
+export const TILE_SIZE = 20;
 
 // Puedes ajustar velocidad global aquí si quieres
 export const DEFAULT_PLAYER_SPEED = 2;
